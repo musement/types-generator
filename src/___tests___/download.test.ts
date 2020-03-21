5fb902e6300a050de24a1ccba4e0ad12011bb0a7
Marco Ziliani <marco.ziliani@musement.com>
2020-03-21 12:17:36 +0100

    fix: make download functional

--- src/___tests___/download.test.ts
+++ src/___tests___/download.test.ts
@@ -1,14 +1,31 @@
 import axios from "axios";
 import { getContent } from "../dowload";
+import { right, left } from "fp-ts/lib/Either";
 
-jest.mock("axios", () =>
-  jest.fn(() => Promise.resolve({ data: { info: { title: "Musement API" } } }))
-);
+jest.mock("axios", () => jest.fn());
 
 describe("getContent", () => {
-  test("it calls axios and returns the data", async () => {
-    const swagger = await getContent("test");
-    expect(axios).toHaveBeenCalledWith({ method: "get", url: "test" });
-    expect(swagger).toEqual({ info: { title: "Musement API" } });
+  describe("when url is reachable", () => {
+    test("it returns a task that calls axios and returns the data", async () => {
+      ((axios as unknown) as jest.Mock).mockResolvedValueOnce({
+        data: { info: { title: "Musement API" } }
+      });
+      const task = getContent("test");
+      const swagger = await task();
+      expect(axios).toHaveBeenCalledWith({ method: "get", url: "test" });
+      expect(swagger).toEqual(right({ info: { title: "Musement API" } }));
+    });
+  });
+
+  describe("when url is unreachable", () => {
+    test("it returns a task that calls axios and returns an error", async () => {
+      ((axios as unknown) as jest.Mock).mockRejectedValueOnce(
+        new Error("error")
+      );
+      const task = getContent("test");
+      const swagger = await task();
+      expect(axios).toHaveBeenCalledWith({ method: "get", url: "test" });
+      expect(swagger).toEqual(left(new Error("error")));
+    });
   });
 });
--- src/dowload.ts
+++ src/dowload.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 import { Swagger } from "./models/Swagger";
+import { Task } from "fp-ts/lib/Task";
+import { Either, left, right } from "fp-ts/lib/Either";
 
-export const getContent = (url: string): Promise<Swagger> =>
-  axios({
-    method: "get",
-    url
-  }).then(response => response.data);
+export function getContent(url: string): Task<Either<Error, Swagger>> {
+  return (): Promise<Either<Error, Swagger>> =>
+    axios({
+      method: "get",
+      url
+    })
+      .then(response => right(response.data))
+      .catch(error => left(error));
+}
