054147e8baa112124dda7d536b8d685f9ec06d4c
Marco Ziliani <marco.ziliani@musement.com>
2020-04-03 16:49:45 +0200

    refactor: use taskify to improve readability

--- src/write.ts
+++ src/write.ts
@@ -1,30 +1,24 @@
 import fs from "fs";
 import prettier from "prettier";
 import * as TE from "fp-ts/lib/TaskEither";
-import * as E from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/pipeable";
 
-function format(types: string): string {
+function prettify(types: string): string {
   return prettier.format(types);
 }
 
 function writeToFile(filename: string) {
   return function(types: string): TE.TaskEither<Error, void> {
-    return async function(): Promise<E.Either<Error, void>> {
-      let error = undefined;
-      await fs.writeFile(filename, types, err => {
-        if (err) {
-          error = err;
-        }
-      });
-      return !error ? E.right(undefined) : E.left(error);
-    };
+    return TE.taskify<string, string, Error, void>(fs.writeFile)(
+      filename,
+      types
+    );
   };
 }
 
 function write(filename: string) {
   return function(types: string): TE.TaskEither<Error, void> {
-    return pipe(types, format, writeToFile(filename));
+    return pipe(types, prettify, writeToFile(filename));
   };
 }
 
